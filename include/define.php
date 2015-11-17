2e95cc58b34f24a6a119640500d4bf5165d17ba1
dirk <dirk@git.imagemagick.org>
2015-11-17 12:58:28 +0100

    Changes for the IM6 website.

--- include/define.php
+++ include/define.php
@@ -5,9 +5,9 @@
   define('DevelopmentTeam', 'development-team@imagemagick.org');
   define('SponsorIcon', 'networkredux.png');
   define('SponsorURL', 'http://www.networkredux.com');
-  define('MagickMajorReleaseText', '7');
-  define('MagickLibVersionText', '7.0.0');
-  define('MagickLibSubversion', '-0');
-  define('PerlMagickVersion', '7.00');
-  define('MagickReleaseDate', '2015-07-04');
+  define('MagickMajorReleaseText', '6');
+  define('MagickLibVersionText', '6.9.2');
+  define('MagickLibSubversion', '-6');
+  define('PerlMagickVersion', '6.92');
+  define('MagickReleaseDate', '2015-10-10');
 ?>
