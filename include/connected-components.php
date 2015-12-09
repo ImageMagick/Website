ca319cc9e16110cd004d2fe69c4a7cdd5488562e
Cristy <urban-warrior@imagemagick.org>
2015-12-09 10:35:18 -0500

    Improve connected component labeling web page

--- include/connected-components.php
+++ include/connected-components.php
@@ -7,33 +7,39 @@
 <pre>
 convert objects.gif -connected-components 4 -auto-level -depth 8 objects.png
 </pre>
-<p>The detected objects are uniquely labeled starting with 1.  Use auto leveling to visualize the detected objects:</p>
+<p>The detected objects are uniquely labeled.  Use auto leveling to visualize the detected objects:</p>
 <ul>
   <a href="<?php echo $_SESSION['RelativePath']?>/../image/objects.png"><img src="<?php echo $_SESSION['RelativePath']?>/../image/objects.png" width="256" height="171" class="image-slices" alt="Objects" /></a>
 </ul>
-<p>Note, the <code>-connected-components</code> option requires ImageMagick version 6.8.9-10 or later.</p>
 <p>Object statistics is useful to review.  To display them, use this command:</p>
 <pre>
 convert objects.gif -define connected-components:verbose=true -connected-components 4 objects.png
 </pre>
-<p>Five objects were detected with these statistics:</p>
+<p>Five objects were detected in the source image with these statistics:</p>
 <pre>
 Objects (id: bounding-box centroid area mean-color):
-  0: 256x171+0+0 +119.2,+80.8 33117 gray(0)
-  2: 120x135+104+18 +159.5,+106.5 8690 gray(255)
-  3: 50x36+129+44 +154.2,+63.4 1529 gray(0)
-  4: 21x23+0+45 +8.8,+55.9 409 gray(255)
-  1: 4x10+252+0 +253.9,+4.1 31 gray(255)
+  0: 256x171+0+0 119.2,80.8 33117 srgb(0,0,0)
+  2: 120x135+104+18 159.5,106.5 8690 srgb(255,255,255)
+  3: 50x36+129+44 154.2,63.4 1529 srgb(0,0,0)
+  4: 21x23+0+45 8.8,55.9 409 srgb(255,255,255)
+  1: 4x10+252+0 253.9,4.1 31 srgb(255,255,255)
 </pre>
-<p>Use <code>-connected-components 8</code> to visit 8 neighbors rather than 4.  By default, neighbor colors must be exact to be part of a unique object. Use <a href="<?php echo $_SESSION['RelativePath']?>/../script/command-line-options.php#fuzz">-fuzz</a> to include pixels that are <var>close</var> in color.</p>
+<p>Use <code>-connected-components 8</code> to visit 8 neighbors rather than 4.  By default, neighbor colors must be exact to be part of a unique object. Use the <a href="<?php echo $_SESSION['RelativePath']?>/../script/command-line-options.php#fuzz">-fuzz</a> option to include pixels as part of an object that are <var>close</var> in color.</p>
 <p>You might want to eliminate small objects by merging them with their larger neighbors.  If so, use this command:</p>
 <pre>
 convert objects.gif -define connected-components:area-threshold=410 -connected-components 4 \
   -auto-level objects.jpg
 </pre>
 <p>Here are the expected results.  Notice, how the small objects are now merged with the background.</p>
-<p class="img-responsive center-block">
+<ul>
   <a href="<?php echo $_SESSION['RelativePath']?>/../image/objects.jpg"><img src="<?php echo $_SESSION['RelativePath']?>/../image/objects.jpg" width="256" height="171" class="image-slices" alt="Objects" /></a>
-</p>
-<p>Use the <a href="<?php echo $_SESSION['RelativePath']?>/../script/command-line-options.php#fuzz">-fuzz</a> option to identify objects that are made up of pixels similar in color (e.g. <code>-fuzz 3%</code>).</p>
+</ul>
+<p>Notice how two of the objects were merged leaving three remaining objects:</p>
+<pre>
+Objects (id: bounding-box centroid area mean-color):
+  0: 256x171+0+0 118.0,80.4 33557 srgb(0,0,0)
+  2: 120x135+104+18 159.5,106.5 8690 srgb(255,255,255)
+  3: 50x36+129+44 154.2,63.4 1529 srgb(0,0,0)
+</pre>
+<p>By default, the labeled image is grayscale.  You can instead replace the object color in the labeled image with the mean-color from the source image. Simply add this setting, <code>-define connected-components:mean-color=true</code>, to your command line.</p>
 </div>
