78931010d971a8bb495585c222ed0a7a82ce3d68
Cristy <urban-warrior@imagemagick.org>
2016-01-01 08:23:48 -0500

    Revert, we'll continue to use -define connected-components:verbose=true instead of -verbose

--- include/connected-components.php
+++ include/connected-components.php
@@ -13,9 +13,8 @@ convert objects.gif -connected-components 4 -auto-level -depth 8 objects.png
 </ul>
 <p>Object statistics is useful to review.  To display them, use this command:</p>
 <pre>
-convert objects.gif -verbose -connected-components 4 objects.png
+convert objects.gif -define connected-components:verbose=true -connected-components 4 objects.png
 </pre>
-<p>Use <code>-define connected-components:verbose=true</code> for verbose output in ImageMagick releases prior to version 6.3.0.</p>
 <p>Five objects were detected in the source image with these statistics:</p>
 <pre>
 Objects (id: bounding-box centroid area mean-color):
