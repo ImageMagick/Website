e595e3f5539056726a5cd51b5010dfb5f17a25f2
Cristy <urban-warrior@imagemagick.org>
2016-05-15 10:33:01 -0400

    ...

--- include/architecture.php
+++ include/architecture.php
@@ -253,16 +253,13 @@ Resource limits:
   &lt;policy domain="resource" name="throttle" value="0"/>
   &lt;policy domain="resource" name="time" value="120"/>
   &lt;policy domain="system" name="precision" value="6"/>
-  &lt;policy domain="cache" name="shared-secret" value="replace with your secret phrase"/>
-  &lt;policy domain="coder" rights="none" pattern="EPHEMERAL" />
-  &lt;policy domain="coder" rights="none" pattern="HTTPS" />
+  &lt;policy domain="cache" name="shared-secret" value="replace with your secret phrase" stealth="true"/>
+  &lt;policy domain="delegate" rights="none" pattern="HTTPS" />
   &lt;policy domain="coder" rights="none" pattern="MVG" />
-  &lt;policy domain="coder" rights="none" pattern="MSL" />
-  &lt;policy domain="coder" rights="none" pattern="TEXT" />
   &lt;policy domain="path" rights="none" pattern="@*" />
 &lt;/policymap>
 </pre>
-<p>Since we process multiple simultaneous sessions, we don't want any one session consuming all the available memory.  Instead large images are cached to disk.  If the image is too large and exceeds the pixel cache disk limit, the program exits.  In addition, we place a time limit to prevent any run-away processing tasks.  Let's prevent possible exploits by removing the right to use certain image coders and indirect reads.</p>
+<p>Since we process multiple simultaneous sessions, we don't want any one session consuming all the available memory.With this policy, large images are cached to disk. If the image is too large and exceeds the pixel cache disk limit, the program exits. In addition, we place a time limit to prevent any run-away processing tasks. If any one image has a width or height that exceeds 8192 pixels, an exception is thrown and processing stops. As of ImageMagick 7.0.1-4 you can prevent the use of any delegate or all delegates (set the pattern to "*") or remove the right to use indirect reads. Note, prior to these releases, use a domain of "coder" to prevent delegate usage (e.g. domain="coder" rights="none" pattern="HTTPS").
 
 <p>Note, the cache limits are global to each invocation of ImageMagick, meaning if you create several images, the combined resource requirements are compared to the limit to determine the pixel cache storage disposition.</p>
 
