8b543331839e8a53e9b804a04fa86696cb28bf10
Cristy <urban-warrior@imagemagick.org>
2016-11-21 19:30:11 -0500

    ...

--- include/api.php
+++ include/api.php
@@ -32,7 +32,7 @@
 
   <dt><a id="julia"></a>Julia</dt>
 
-<dd><a href="https://github.com/JuliaIO/ImageMagick.jl">Julia</a> bindings to ImageMagick.</dd>
+<dd><a href="https://github.com/JuliaIO/ImageMagick.jl">JuliaIO</a> provides an object-oriented Julia interface to ImageMagick.</dd>
 
   <dt><a id="labview"></a>LabVIEW</dt>
 
