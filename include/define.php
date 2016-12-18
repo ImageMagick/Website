6b65683776bcdaea1e58998e80d4f760949a0c5a
Cristy <urban-warrior@imagemagick.org>
2016-12-18 11:06:31 -0500

    Remove script until XSS vulnerability is fixed

--- include/define.php
+++ include/define.php
@@ -5,9 +5,9 @@
   define('DevelopmentTeam', 'development-team@imagemagick.org');
   define('SponsorIcon', 'networkredux.png');
   define('SponsorURL', 'http://www.networkredux.com');
-  define('MagickMajorReleaseText', '6');
-  define('MagickLibVersionText', '6.9.2');
-  define('MagickLibSubversion', '-6');
-  define('PerlMagickVersion', '6.92');
-  define('MagickReleaseDate', '2015-10-10');
+  define('MagickMajorReleaseText', '7');
+  define('MagickLibVersionText', '7.0.4');
+  define('MagickLibSubversion', '-0');
+  define('PerlMagickVersion', '7.04');
+  define('MagickReleaseDate', '2016-12-18');
 ?>
