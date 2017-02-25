53bb327db15ddb7410da7968b3577fba200f119c
Cristy <urban-warrior@imagemagick.org>
2017-02-25 10:37:59 -0500

    ...

--- include/layout.php
+++ include/layout.php
@@ -81,7 +81,7 @@
   </footer>
 </div><!-- /.container -->
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="<?php echo $_SESSION['RelativePath']?>/../js/magick.php"></script>
 </div>
 </body>
