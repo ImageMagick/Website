a18ade555ef30c47c20681f280d5ef099430d236
Cristy <urban-warrior@imagemagick.org>
2017-03-02 20:13:39 -0500

    ...

--- include/download.php
+++ include/download.php
@@ -135,9 +135,9 @@ sudo port install ImageMagick
   </tr>
 
   <tr>
-    <td>ImageMagick-x86_64-apple-darwin15.6.0.tar.gz</td>
-    <td><a href="https://www.imagemagick.org/download/binaries/ImageMagick-x86_64-apple-darwin15.6.0.tar.gz">download</a></td>
-    <td><a href="ftp://ftp.imagemagick.org/pub/ImageMagick/binaries/ImageMagick-x86_64-apple-darwin15.6.0.tar.gz">download</a></td>
+    <td>ImageMagick-x86_64-apple-darwin16.4.0.tar.gz</td>
+    <td><a href="https://www.imagemagick.org/download/binaries/ImageMagick-x86_64-apple-darwin16.4.0.tar.gz">download</a></td>
+    <td><a href="ftp://ftp.imagemagick.org/pub/ImageMagick/binaries/ImageMagick-x86_64-apple-darwin16.4.0.tar.gz">download</a></td>
     <td>Mac OS X El Capitan</td>
   </tr>
 </table></div>
