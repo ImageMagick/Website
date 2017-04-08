b7a8bcfa65b24fea411c24303c22d2eb3e0b22a8
Cristy <urban-warrior@imagemagick.org>
2017-04-08 19:05:31 -0400

    ...

--- include/security-policy.php
+++ include/security-policy.php
@@ -46,8 +46,9 @@ convert: no images defined `wizard.jpg'
   &lt;policy domain="coder" rights="read|write" pattern="{GIF,JPEG,PNG,WEBP}" />
 </pre>
 
-<p>As of ImageMagick 7.0.5-4, you can allocate the pixel cache with anonymous memory mapping rather than from heap.  As a consequence, the pixels are initialized to zero.  You can also securely delete any temporary files for increased security.  The value is the number of times to shred (replace its content with random data before deleting) a temporary file.  For example,</p>
+<p>As of ImageMagick 7.0.5-5, you can allocate the pixel cache and some internal buffers with anonymous memory mapping rather than from heap.  As a consequence, the pixels are initialized to zero.  You can also securely delete any temporary files for increased security.  The value is the number of times to shred (replace its content with random data before deleting) a temporary file.  For example,</p>
 <pre>
+  &lt;policy domain="system" name="memory-map" value="anonymous"/>
   &lt;policy domain="cache" name="memory-map" value="anonymous"/>
   &lt;policy domain="system" name="shred" value="1"/>
 </pre>
