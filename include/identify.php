3dcfbf0f0025e7829d0a5f6eac230fd087f7ee28
Cristy <urban-warrior@imagemagick.org>
2017-05-17 06:04:01 -0400

    ...

--- include/identify.php
+++ include/identify.php
@@ -14,7 +14,7 @@ rose.jpg JPEG 70x46 70x46+0+0 8-bit sRGB 2.36KB 0.000u 0:00.000
 
 <p>Next, we look at the same image in greater detail:</p>
 
-<pre class="pre-scrollable">-> identify -verbose rose.jpg
+<pre class="pre-scrollable">-> magick identify -verbose rose.jpg
 Image: rose.jpg
   Format: JPEG (Joint Photographic Experts Group JFIF format)
   Mime type: image/jpeg
