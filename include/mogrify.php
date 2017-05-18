48f0c38dd85809056b53789ea1558d79d2a1b98f
Cristy <urban-warrior@imagemagick.org>
2017-05-18 08:56:06 -0400

    ...

--- include/mogrify.php
+++ include/mogrify.php
@@ -1,7 +1,7 @@
 <div class="magick-header">
 <p class="text-center"><a href="#usage">Example Usage</a> • <a href="#options">Option Summary</a></p>
 
-<p class="lead magick-description">Use the <code>mogrify</code> program to resize an image, blur, crop, despeckle, dither, draw on, flip, join, re-sample, and much more.   This tool is similar to <a href="<?php echo $_SESSION['RelativePath']?>/../script/convert.php">convert</a> except that the original image file is overwritten (unless you change the file suffix with the <a href="<?php echo $_SESSION['RelativePath']?>/../script/command-line-options.php#format">-format</a> option) with any changes you request.  See <a href="<?php echo $_SESSION['RelativePath']?>/../script/command-line-processing.php">Command Line Processing</a> for advice on how to structure your <code>mogrify</code> command or see below for sample usages of the command.</p>
+<p class="lead magick-description">Use the <code>mogrify</code> program to resize an image, blur, crop, despeckle, dither, draw on, flip, join, re-sample, and much more.   This tool is similar to <a href="<?php echo $_SESSION['RelativePath']?>/../script/convert.php">convert</a> except that the original image file is <em>overwritten</em> (unless you change the file suffix with the <a href="<?php echo $_SESSION['RelativePath']?>/../script/command-line-options.php#format">-format</a> option) with any changes you request.  See <a href="<?php echo $_SESSION['RelativePath']?>/../script/command-line-processing.php">Command Line Processing</a> for advice on how to structure your <code>mogrify</code> command or see below for sample usages of the command.</p>
 
 <h2 class="magick-header"><a id="usage"></a>Example Usage</h2>
 
