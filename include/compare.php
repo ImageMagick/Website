6fdf5188f513147d10d32644dff111aa7d71e4f2
Cristy <urban-warrior@imagemagick.org>
2017-05-19 07:18:58 -0400

    ...

--- include/compare.php
+++ include/compare.php
@@ -241,6 +241,11 @@ transparent, extract, background, or shape the alpha channel</td>
   </tr>
 
   <tr>
+    <td><a href="<?php echo $_SESSION['RelativePath']?>/../script/command-line-options.php#repage">-repage <var>geometry</var></a></td>
+    <td>size and location of an image canvas</td>
+  </tr>
+
+  <tr>
     <td><a href="<?php echo $_SESSION['RelativePath']?>/../script/command-line-options.php#resize">-resize <var>geometry</var></a></td>
     <td>resize the image</td>
   </tr>
