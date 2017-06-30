8ec14a681ad057ffcd419f4280a39492a5763099
Cristy <urban-warrior@imagemagick.org>
2017-06-30 09:52:09 -0400

    ...

--- class/MetaContent.php
+++ class/MetaContent.php
@@ -14,26 +14,26 @@
       $keywords=str_replace(',','',$keywords);
       $keywords=str_replace('and ','',$keywords);
       $meta_words=str_replace(' ',', ',$keywords);
-      $meta="<meta charset=\"utf-8\" />\n";
-      $meta.="  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\" />\n";
+      $meta="<meta charset=\"utf-8\" >\n";
+      $meta.="  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\" >\n";
       if (empty($title))
         $meta.="  <title>$this->slogan @ $this->sitename</title>\n";
       else
         $meta.="  <title>$title @ $this->sitename</title>\n";
-      $meta.="  <meta name=\"application-name\" content=\"$this->application_name\"/>\n";
-      $meta.="  <meta name=\"description\" content=\"$this->description\"/>\n";
-      $meta.="  <meta name=\"application-url\" content=\"$this->application_url\"/>\n";
-      $meta.="  <meta name=\"generator\" content=\"$this->generator\"/>\n";
-      $meta.="  <meta name=\"keywords\" content=\"$meta_words, $this->keywords2\"/>\n";
-      $meta.="  <meta name=\"rating\" content=\"GENERAL\"/>\n";
-      $meta.="  <meta name=\"robots\" content=\"INDEX, FOLLOW\"/>\n";
-      $meta.="  <meta name=\"generator\" content=\"$this->company_name\"/>\n";
-      $meta.="  <meta name=\"author\" content=\"$this->company_name\"/>\n";
-      $meta.="  <meta name=\"revisit-after\" content=\"2 DAYS\"/>\n";
-      $meta.="  <meta name=\"resource-type\" content=\"document\"/>\n";
-      $meta.="  <meta name=\"copyright\" content=\"Copyright (c) 1999-2017 $this->company_name\"/>\n";
-      $meta.="  <meta name=\"distribution\" content=\"Global\"/>\n";
-      $meta.="  <meta name=\"magick-serial\" content=\"P131-S030410-R485315270133-P82224-A6668-G1245-1\"/>\n";
+      $meta.="  <meta name=\"application-name\" content=\"$this->application_name\">\n";
+      $meta.="  <meta name=\"description\" content=\"$this->description\">\n";
+      $meta.="  <meta name=\"application-url\" content=\"$this->application_url\">\n";
+      $meta.="  <meta name=\"generator\" content=\"$this->generator\">\n";
+      $meta.="  <meta name=\"keywords\" content=\"$meta_words, $this->keywords2\">\n";
+      $meta.="  <meta name=\"rating\" content=\"GENERAL\">\n";
+      $meta.="  <meta name=\"robots\" content=\"INDEX, FOLLOW\">\n";
+      $meta.="  <meta name=\"generator\" content=\"$this->company_name\">\n";
+      $meta.="  <meta name=\"author\" content=\"$this->company_name\">\n";
+      $meta.="  <meta name=\"revisit-after\" content=\"2 DAYS\">\n";
+      $meta.="  <meta name=\"resource-type\" content=\"document\">\n";
+      $meta.="  <meta name=\"copyright\" content=\"Copyright (c) 1999-2017 $this->company_name\">\n";
+      $meta.="  <meta name=\"distribution\" content=\"Global\">\n";
+      $meta.="  <meta name=\"magick-serial\" content=\"P131-S030410-R485315270133-P82224-A6668-G1245-1\">\n";
       return($meta);
     }
   }
