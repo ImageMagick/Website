5f01bc5572fbe66e758cfcefc05dae419a01acb4
Cristy <urban-warrior@imagemagick.org>
2017-07-08 14:08:24 -0400

    ...

--- include/command-line-options.php
+++ include/command-line-options.php
@@ -411,6 +411,8 @@ href="#auto-orient" >-auto-orient</a> operator will do nothing. </p>
 <dt class="col-md-4">OTSU</dt><dd class="col-md-8">cluster-based image thresholding.</dd>
 <dt class="col-md-4">Triangle</dt><dd class="col-md-8">a geometric thresholding method.</dd>
 
+<p>The computed threshold is returned as the <kbd>auto-threshold:threshold</kbd> image property.</p>
+
 <div style="margin: auto;">
   <h3><a id="average"></a>-average</h3>
 </div>
