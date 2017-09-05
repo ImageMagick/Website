a1478220185c015ea79a9876294c35b909f72f15
Cristy <urban-warrior@imagemagick.org>
2017-09-05 12:47:01 -0400

    ...

--- include/import.php
+++ include/import.php
@@ -1,7 +1,7 @@
 <div class="magick-header">
 <p class="text-center"><a href="#usage">Example Usage</a> • <a href="#options">Option Summary</a></p>
 
-<p class="lead magick-description">Use the <code>import</code> program to capture some or all of an X server screen and save the image to a file. <code>import</code> captures the window selected by clicking or program argument. See <a href="command-line-processing.php">Command Line Processing</a> for advice on how to structure your <code>import</code> command or see below for example usages of the command.</p>
+<p class="lead magick-description">Use the <code>import</code> program to capture some or all of an X server screen and save the image to a file. <code>import</code> captures the window selected by clicking or program argument. See <a href="<?php echo $_SESSION['RelativePath']?>/../script/command-line-processing.php">Command Line Processing</a> for advice on how to structure your <code>import</code> command or see below for example usages of the command.</p>
 
 <h2 class="magick-post-title"><a id="usage"></a>Example Usage</h2>
 
