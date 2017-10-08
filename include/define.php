fa09d67bd8da071b9f3f2b0bee831357f8703bc4
Cristy <urban-warrior@imagemagick.org>
2017-10-08 17:17:45 -0400

    ...

--- include/define.php
+++ include/define.php
@@ -6,8 +6,8 @@
   define('SponsorIcon', 'networkredux.png');
   define('SponsorURL', 'http://www.networkredux.com');
   define('MagickMajorReleaseText', '7');
-  define('MagickLibVersionText', '7.0.4');
-  define('MagickLibSubversion', '-0');
-  define('PerlMagickVersion', '7.04');
-  define('MagickReleaseDate', '2016-12-18');
+  define('MagickLibVersionText', '7.0.7');
+  define('MagickLibSubversion', '-7');
+  define('PerlMagickVersion', '7.07');
+  define('MagickReleaseDate', '2017-10-08');
 ?>
