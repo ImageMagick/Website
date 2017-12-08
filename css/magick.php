af8e7aa366bafcffe411b41974e14fdede5f4b4a
Cristy <urban-warrior@imagemagick.org>
2017-12-08 12:06:46 -0500

    ...

--- css/magick.php
+++
@@ -1,9 +0,0 @@
-<?php
-  ob_start("ob_gzhandler");
-  header("Cache-Control: public");
-  header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-  header('Expires: ' . gmdate('D, d M Y H:i:s',time()+604800) . ' GMT');
-  header("Content-type: text/css");
-  readfile('magick.css');
-  ob_end_flush();
-?>
--- include/layout.php
+++ include/layout.php
@@ -26,44 +26,64 @@
 <html lang="en">
 <head>
   <?php echo $meta->metadata($title); ?>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta name="google-site-verification" content="_bMOCDpkx9ZAzBwb2kF3PRHbfUUdFj2uO8Jd1AXArz4">
   <link href="https://www.imagemagick.org<?php echo $_SERVER['SCRIPT_NAME']?>" rel="canonical">
   <link href="<?php echo $_SESSION['RelativePath']?>/../image/wand.png" rel="icon">
   <link href="<?php echo $_SESSION['RelativePath']?>/../image/wand.ico" rel="shortcut icon">
-  <link href="<?php echo $_SESSION['RelativePath']?>/../css/magick.php" rel="stylesheet">
+  <link href="<?php echo $_SESSION['RelativePath']?>/../assets/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo $_SESSION['RelativePath']?>/../assets/magick-template.css" rel="stylesheet">
 </head>
 <?php flush(); ?>
 <body>
-<div class="magick-masthead">
-  <div class="container">
-    <?php if ($_SERVER["SERVER_ADDR"] != "10.144.245.30") echo '<script async="async" src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>' ?>
-    <ins class="adsbygoogle"
-         style="display:block"
-         data-ad-client="ca-pub-3129977114552745"
-         data-ad-slot="6345125851"
-         data-ad-format="auto"></ins>
-    <script>
-      (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-    <nav class="nav magick-nav">
-      <a class="nav-link <?php IsActive($title,"Convert, Edit, Or Compose Bitmap Images"); ?>" href="<?php echo $_SESSION['RelativePath']?>/../index.php">Home</a>
-      <a class="nav-link <?php IsActive($title,"Download"); ?>" href="<?php echo $_SESSION['RelativePath']?>/../script/download.php">Download</a>
-      <a class="nav-link <?php IsActive($title,"Command-line Tools"); ?>" href="<?php echo $_SESSION['RelativePath']?>/../script/command-line-tools.php">Tools</a>
-      <a class="nav-link <?php IsActive($title,"Command-line Processing"); ?>" href="<?php echo $_SESSION['RelativePath']?>/../script/command-line-processing.php">Command-line</a>
-      <a class="nav-link <?php IsActive($title,"Resources"); ?>" href="<?php echo $_SESSION['RelativePath']?>/../script/resources.php">Resources</a>
-      <a class="nav-link <?php IsActive($title,"Develop"); ?>" href="<?php echo $_SESSION['RelativePath']?>/../script/develop.php">Develop</a>
-      <a class="nav-link <?php IsActive($title,"Search"); ?>" href="<?php echo $_SESSION['RelativePath']?>/../script/search.php">Search</a>
-      <a class="nav-link float-right" href="https://www.imagemagick.org/discourse-server/">Community</a>
-    </nav>
-  </div>
-</div>
-<div class="container">
+  <header>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="https://www.imagemagick.org/#">ImageMagick</a>
+    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="navbar-collapse collapse" id="navbarsExampleDefault" style="">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item <?php IsActive($title,"Convert, Edit, Or Compose Bitmap Images"); ?>">
+        <a class="nav-link" href="<?php echo $_SESSION['RelativePath']?>/../index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item <?php IsActive($title,"Download"); ?>">
+        <a class="nav-link" href="<?php echo $_SESSION['RelativePath']?>/../script/download.php">Download</a>
+      </li>
+      <li class="nav-item <?php IsActive($title,"Command-line Tools"); ?>">
+        <a class="nav-link" href="<?php echo $_SESSION['RelativePath']?>/../script/command-line-tools.php">Tools</a>
+      </li>
+      <li class="nav-item <?php IsActive($title,"Command-line Processing"); ?>">
+        <a class="nav-link" href="<?php echo $_SESSION['RelativePath']?>/../script/command-line-processing.php">Command-line</a>
+      </li>
+      <li class="nav-item <?php IsActive($title,"Resources"); ?>">
+        <a class="nav-link" href="<?php echo $_SESSION['RelativePath']?>/../script/resources.php">Resources</a>
+      </li>
+      <li class="nav-item <?php IsActive($title,"Develop"); ?>">
+        <a class="nav-link" href="<?php echo $_SESSION['RelativePath']?>/../script/develop.php">Develop</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://www.imagemagick.org/discourse-server/">Community</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0" action="<?php echo $_SESSION['RelativePath']?>/../script/search.php">
+      <!-- <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"> -->
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    </div>
+  </nav>
+  </header>
+  <main role="main" class="container">
+    <div class="magick-template">
 <?php
   }
 
   function SiteFooter()
   {
 ?>
+    </div>
+  </main><!-- /.container -->
   <footer class="magick-footer">
     <p><a href="<?php echo $_SESSION['RelativePath']?>/../script/support.php">Donate</a> •
      <a href="<?php echo $_SESSION['RelativePath']?>/../script/sitemap.php">Sitemap</a> •
@@ -75,13 +95,15 @@
     <a href="http://pgp.mit.edu:11371/pks/lookup?op=get&amp;search=0x89AB63D48277377A">Public Key</a> •
     <a href="<?php echo $_SESSION['RelativePath']?>/../script/contact.php">Contact Us</a></p>
     <?php if ($_SERVER["SERVER_ADDR"] == "10.144.245.30") echo '<p><small>Sponsored by <a href="https://transloadit.com">Transloadit</a> - the file uploading &amp; processing service</small></p>' ?>
-    <p><small>© 1999-2017 ImageMagick Studio LLC</small></p>
+    <p><small>© 1999-2018 ImageMagick Studio LLC</small></p>
   </footer>
-</div><!-- /.container -->
 
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-  <script src="<?php echo $_SESSION['RelativePath']?>/../js/magick.php"></script>
+  <!-- Javascript assets -->
+  <script src="<?php echo $_SESSION['RelativePath']?>/../assets/jquery-3.2.1.slim.min.js" crossorigin="anonymous"></script>
+  <script src="<?php echo $_SESSION['RelativePath']?>/../assets/popper.min.js" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="<?php echo $_SESSION['RelativePath']?>/../assets/jquery.min.js"><\/script>')</script>
+  <script src="<?php echo $_SESSION['RelativePath']?>/../assets/bootstrap.min.js"></script>
+
 </body>
 </html>
 <?php
