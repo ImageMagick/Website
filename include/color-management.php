281f2fee84686f0b7b5f1569522c35d10a3108c4
Frederick Weinhaus <fred@Fredericks-Mac-mini.local>
2017-12-28 10:55:05 -0800

    Edited color-management.php for typo

--- include/color-management.php
+++ include/color-management.php
@@ -20,7 +20,7 @@
 > options:</p>
 
 <pre class="highlight"><code>
-convert myimage.png --colorspace LinearGray myRGBimage.png
+convert myimage.png -colorspace LinearGray myRGBimage.png
 convert myimage.png -colorspace RGB -colorspace Gray myRGBimage.png
 convert myimage.png -intensity Rec709luminance -colorspace gray myRGBimage.png
 convert myimage.png -grayscale Rec709luminance myRGBimage.png
