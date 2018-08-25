1d291d446e4c58c76d5ce528af5b072f2a7e6198
Cristy <urban-warrior@imagemagick.org>
2018-08-25 17:33:49 -0400

    ...

--- include/command-line-processing.php
+++ include/command-line-processing.php
@@ -397,7 +397,7 @@ setting for more specifics.</p>
     <td><var>area</var>@</td>
     <td>Resize image to have specified area in pixels. Aspect ratio is preserved.</td>
   <tr>
-    <td><var>x</var>:<var>y</var>~</td>
+    <td><var>x</var>:<var>y</var></td>
     <td>Here x and y denotes an aspect ratio (e.g. 3:2 = 1.5).</td>
   </tr>
   </tr>
