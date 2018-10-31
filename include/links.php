fa7dfc8dc14fe56ebbcea0f4bf0b14870911a289
Cristy <urban-warrior@imagemagick.org>
2018-10-31 08:37:17 -0400

    ...

--- include/links.php
+++ include/links.php
@@ -6,7 +6,7 @@
 <h2><a class="anchor" id="web"></a>Use ImageMagick Online</h2>
 <ul>
   <dd class="col-md-8"><a href="http://jqmagick.imagemagick.org/">JqMagick</a></dd>
-  <dd class="col-md-8"><a href="https://imagemagick.org/MagickStudio/">Magick Studio</a></dd>
+  <dd class="col-md-8"><a href="https://www.imagemagick.org/MagickStudio/">Magick Studio</a></dd>
 </ul>
 
 <h2><a class="anchor" id="command-line"></a>Command-line Tutorials</h2>
