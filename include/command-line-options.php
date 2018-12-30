aba1cf4e4486591e81e9d2c40211359d9caff410
Cristy <urban-warrior@imagemagick.org>
2018-12-29 21:14:31 -0500

    ...

--- include/command-line-options.php
+++ include/command-line-options.php
@@ -949,7 +949,7 @@ sides of the image.  </p>
 href="#splice">-splice</a> that was given the same <var>geometry</var> and <a href="#gravity">-gravity</a> settings. </p>
 
 <div style="margin: auto;">
-  <h3><a class="anchor" id="clahe"></a>-clahe <var>x-tiles</var><br />-clahe <var>width</var>x<var>height</var>+<var>number-bins</var>+<var>clip-limit</var></h3>
+  <h3><a class="anchor" id="clahe"></a>-clahe <var>width</var>x<var>height</var>+<var>number-bins</var>+<var>clip-limit</var></h3>
 </div>
 
 <p class="magick-description">contrast limited adaptive histogram equalization. The image is divided into tiles of <var>width</var> and <var>height</var> pixels.  <var>number-bins</var> is the number of histogram bins per tile (min 2, max 256).  <var>clip-limit</var> is the contrast limit for localised changes in contrast. A clip-limit of 2 to 3 is a good starting place (e.g. -clahe 50x50%+128+3).</p>
