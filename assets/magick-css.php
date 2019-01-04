4360e219d5cc5f1c30c22af0bd2671140d4e0497
Cristy <urban-warrior@imagemagick.org>
2019-01-04 11:39:45 -0500

    ...

--- assets/magick-css.php
+++ assets/magick-css.php
@@ -3,7 +3,7 @@
   header("Cache-Control: public");
   header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
   header('Expires: ' . gmdate('D, d M Y H:i:s',time()+604800) . ' GMT');
-  header("Content-type: text/css");
+  header("Content-type: text/css; charset=utf-8");
   readfile('bootstrap.min.css');
   readfile('magick-template.css');
   ob_end_flush();
--- assets/magick-js.php
+++ assets/magick-js.php
@@ -3,7 +3,7 @@
   header("Cache-Control: public");
   header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
   header('Expires: ' . gmdate('D, d M Y H:i:s',time()+604800) . ' GMT');
-  header("Content-type: text/javascript");
+  header("Content-type: text/javascript; charset=utf-8");
   readfile('jquery-3.3.1.slim.min.js');
   readfile('popper.min.js');
   readfile('bootstrap.min.js');
