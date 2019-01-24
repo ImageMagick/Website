abb1941f9e33c3212a11be0b1c6ab31e3457a2d5
Dirk Lemstra <dirk@lemstra.org>
2019-01-24 21:58:42 +0100

    Corrected url.

--- include/sitemap.php
+++ include/sitemap.php
@@ -73,7 +73,7 @@
   <li> <dl class="magick-list-unstyled">
     <li><a href="https://imagemagick.org/ChMagick">ChMagick</a>: is a <a href="http://www.softintegration.com/">Ch</a> an embeddable MagickCore C/C++ interpreter for cross-platform scripting.</li>
     <li><a href="http://common-lisp.net/project/cl-magick/">CL-Magick</a>: provides a Common Lisp interface to the ImageMagick library.</li>
-    <li><a href="http://magick.codeplex.com/">Magick.NT</a>: Use Magick.NET to convert, compose, and edit images from Windows .NET.</li>
+    <li><a href="https://github.com/dlemstra/Magick.NET">Magick.NET</a>: Use Magick.NET to convert, compose, and edit images from Windows .NET.</li>
     <li><a href="https://imagemagick.org/Magick++">Magick++</a>: provides an object-oriented C++ interface to ImageMagick.</li>
     <li><a href="http://pecl.php.net/package/imagick">IMagick</a>: is a native PHP extension to create and modify images using the ImageMagick API.</li>
     <li><a href="https://github.com/techblue/jmagick">JMagick</a>: provides an object-oriented Java interface to ImageMagick.</li>
