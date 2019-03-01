2dbc47d1e00ca10061b9acddcf00fd66d13e312e
Cristy <urban-warrior@imagemagick.org>
2019-03-01 07:03:57 -0500

    ...

--- assets/magick-js.php
+++ assets/magick-js.php
@@ -1,6 +1,7 @@
 <?php
   ob_start("ob_gzhandler");
   header("Cache-Control: public");
+  header("Cache-Control: max-age=604800");
   header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
   header('Expires: ' . gmdate('D, d M Y H:i:s',time()+604800) . ' GMT');
   header("Content-type: text/javascript; charset=utf-8");
