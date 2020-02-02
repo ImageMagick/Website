6deca8792b2e0a18da21afc7a987cb3eea46d0c0
Cristy <urban-warrior@imagemagick.org>
2020-02-01 19:12:43 -0500

    ...

--- include/connected-components.php
+++ include/connected-components.php
@@ -33,6 +33,7 @@
   0: 256x171+0+0 118.0,80.4 33557 srgb(0,0,0)
   2: 120x135+104+18 159.5,106.5 8690 srgb(255,255,255)
   3: 50x36+129+44 154.2,63.4 1529 srgb(0,0,0)</code></pre>
+<p>Area thresholding does not always work if objects are stacked on top of one-another.  We're seeking a robust algorithm to tackle this use case.</p>
 <p>By default, the labeled image is grayscale.  You can instead replace the object color in the labeled image with the mean-color from the source image. Simply add this setting, <code>-define connected-components:mean-color=true</code>, to your command line.</p>
 <p>Thresholds can optionally include ranges, e.g. <code>-define connected-components:area-threshold=410-1600</code>. To keep the background object, identify it with <code>-define connected-components:background-id=<var>object-id</var></code>.  The default background object is the object with the largest area.</p>
 <p>You may want to remove certain objects by making them transparent.  Use <code>-define connected-components:remove-ids=<em>list-of-ids</em></code> (e.g. -define connected-components:remove-ids=2,4-5).  Or use <code>-define connected-components:keep-ids=<em>list-of-ids</em></code> to keep these objects and make all others transparent. For convenience, you can keep the top objects with this option: <code>-define connected-components:keep-top=<em>number-of-objects</em></code>. Rather than object ids, you can instead remove or keep objects by colors, e.g. <code>-define connected-components:keep-colors=<em>red;green;blue</em></code>.</p>
