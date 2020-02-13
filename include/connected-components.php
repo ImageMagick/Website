acf9e9c1cfad28b0624d0e493276e5e714ef01bc
Cristy <urban-warrior@imagemagick.org>
2020-02-12 20:20:11 -0500

    ...

--- include/connected-components.php
+++ include/connected-components.php
@@ -37,6 +37,7 @@
 <p>Thresholds can optionally include ranges, e.g. <code>-define connected-components:area-threshold=410-1600</code>. To keep the background object, identify it with <code>-define connected-components:background-id=<var>object-id</var></code>.  The default background object is the object with the largest area.</p>
 <p>We support other thresholding metrics,  They include:</p>
 <ul>
+<li>connected-components:circularity-threshold</li>
 <li>connected-components:perimeter-threshold</li>
 </ul>
 <p>You may want to remove certain objects.  Use <code>-define connected-components:remove-ids=<em>list-of-ids</em></code> (e.g. -define connected-components:remove-ids=2,4-5).  Or use <code>-define connected-components:keep-ids=<em>list-of-ids</em></code> to keep these objects and merge all others. For convenience, you can keep the top objects with this option: <code>-define connected-components:keep-top=<em>number-of-objects</em></code>. Rather than object ids, you can instead remove or keep objects by colors, e.g. <code>-define connected-components:keep-colors=<em>red;green;blue</em></code>.</p>
