366f57be0d1dbed39350effcd335e574e4f7ef73
Cristy <urban-warrior@imagemagick.org>
2020-03-21 11:30:15 -0400

    ...

--- include/escape.php
+++ include/escape.php
@@ -424,6 +424,10 @@ when enclosed in braces (long form):</p>
     <td>CALCULATED: minimum value statistic of image</td>
   </tr>
   <tr>
+    <td><a href="<?php echo $_SESSION['RelativePath']?>/../script/convex-hull.php">%[minimum-bounding-rectangle]</a></td>
+    <td>points that form the minimum bounding rectangle around the foreground object.  Use <code><a href="#define">-define</a> convex-hull:background-color=<em>color</em></code> to identify the image background color.</td>
+  </tr>
+  <tr>
     <td>%[opaque]</td>
     <td>CALCULATED: is image fully-opaque?</td>
   </tr>
