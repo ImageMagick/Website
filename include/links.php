d0a9c66049d766162f720454ac56662cc10e7171
Cristy <urban-warrior@imagemagick.org>
2020-09-17 22:44:13 +0000

    ...

--- include/links.php
+++ include/links.php
@@ -1,5 +1,5 @@
 <div class="magick-header">
-<p class="text-center"><a href="#web">Use ImageMagick on the Web</a> • <a href="#command-line">Command-line Tutorials</a> • <a href="#program-interfaces">Program Interface Tutorials</a> • <a href="#techniques">ImageMagick Techniques</a> • <a href="#install">Installation Tutorials</a> • <a href="#topics">ImageMagick Topics</a> • <a href="#book-review">ImageMagick Book Review</a> • <a href="#mirrors">Web Site Mirrors</a> • <a href="#bank">Image Bank</a> • <a href="#other-projects">Other Projects</a></p>
+<p class="text-center"><a href="#command-line">Command-line Tutorials</a> • <a href="#program-interfaces">Program Interface Tutorials</a> • <a href="#techniques">ImageMagick Techniques</a> • <a href="#install">Installation Tutorials</a> • <a href="#topics">ImageMagick Topics</a> • <a href="#book-review">ImageMagick Book Review</a> • <a href="#mirrors">Web Site Mirrors</a> • <a href="#bank">Image Bank</a> • <a href="#other-projects">Other Projects</a></p>
 
 <p class="lead magick-description">Listed here are a number of external web sites that are related to ImageMagick.  ImageMagick Studio does not maintain or endorse these sites, excepting the Wizard's Toolkit site, but we feel they are a helpfull adjunct to this web site.</p>
 
