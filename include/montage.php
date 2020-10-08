f8418a94ff4038cd1d5c786983948088be14d927
Cristy <urban-warrior@imagemagick.org>
2020-10-08 00:28:05 +0000

    ...

--- include/montage.php
+++ include/montage.php
@@ -44,14 +44,9 @@
 <p>You can find additional examples of using <code>montage</code> at <a href="https://legacy.imagemagick.org/Usage/montage/">Examples of ImageMagick Usage</a> and <a href="http://www.ibm.com/developerworks/library/l-graf/?ca=dnt-428">Graphics from the Command Line</a>.  Further discussion is available in  <a href="http://www.ibm.com/developerworks/library/l-graf2/?ca=dgr-lnxw15GraphicsLine">More Graphics from the Command Line</a> and <a href="https://legacy.imagemagick.org/Usage/">Examples of ImageMagick Usage</a>.</p>
 
 <h5>Ashlar Pseudo-image Format</h5>
-<p>An alternative to the <code>montage</code> tool is the Ashlar pseudo-image format.  The montage layout is composed of a regular grid pattern aligned both verticall and horizontally in left-to-right order of the images you provide.  The Ashlar format differs from <code>montage</code> in these ways:</p>
-<ul>
-<li>it is a pseudo-image format rather than a command-line tool;</li>
-<li>by default, only the vertical edges are aligned;</li>
-<li>the image order and layout is adjusted to optimize the canvas space.</li>
-</ul>
-<p> For example:</p>
-<pre class="highlight"><code>convert logo: wizard: rose: 'ashlar:canvas.png[1024x768+4+4]'</code></pre>
+<p>Use the Ashlar pseudo-image format to lay out an image sequence in continuous irregular courses. By default, a reasonable canvas size and border width is determined relative to the image collection you provide. You can explicitedly set the canvas size and border width by appending to the filename, e.g. ashlar:canvas.png[1024x768+4+4]. By default, alignment is along the left edge. Use -define ashlar:best-fit=true to align on both the left and right edges. You can label the image tiles with, for example, -label %f. Here is an example command:
+<pre class="highlight"><code>convert '*.png' -resize 320x320 -label %f ashlar:ashlar.png</code></pre>
+<p>This is designed to quickly view a collection of images. To have more control over the layout of your image tiles, use the <code>montage</code> command instead.</p>
 
 <h2><a class="anchor" id="options"></a>Option Summary</h2>
 
