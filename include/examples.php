016d5d32a423c319d92a3b703dd216063b453802
Cristy <urban-warrior@imagemagick.org>
2020-10-13 23:26:43 +0000

    ...

--- include/examples.php
+++ include/examples.php
@@ -1,5 +1,5 @@
 <div class="magick-header">
-<p class="lead magick-description">Here are a few examples of what you can do with an image using ImageMagick from the command line, a program interface, or script.  You can generate this image yourself with this <a href="<?php echo $_SESSION['RelativePath']?>/../script/perl-magick.php">PerlMagick</a> script, <a href="<?php echo $_SESSION['RelativePath']?>/../source/examples.pl">examples.pl</a>.</p>
+<p class="lead magick-description">These examples of ImageMagick usage illustrate what you can do with an image from the command line, a program interface, or script.  You can generate this image yourself with this <a href="<?php echo $_SESSION['RelativePath']?>/../script/perl-magick.php">PerlMagick</a> script, <a href="<?php echo $_SESSION['RelativePath']?>/../source/examples.pl">examples.pl</a>.</p>
 
 <p><img class="img-fluid mx-auto d-block" src="<?php echo $_SESSION['RelativePath']?>/../image/examples.jpg" alt="[ImageMagick Examples]" width="734" height="2972" name="imagemagick-examples" /></p>
 </div>
