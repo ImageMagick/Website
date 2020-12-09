3ccf9f8bb5ce95ff99028f4ef2f691e39921ee28
Cristy <urban-warrior@imagemagick.org>
2020-12-09 19:22:04 +0000

    ...

--- include/cite.php
+++ include/cite.php
@@ -3,13 +3,13 @@
 <p class="lead magick-description">Learn how to cite ImageMagick in your work.  Choose from these citation formats:</p>
 
 <h5>Citation in APA style</h5>
-<p class="highlight">The ImageMagick Development Team. (2020). ImageMagick. Retrieved from https://imagemagick.org</p>
+<p class="highlight">The ImageMagick Development Team. (2021). ImageMagick. Retrieved from https://imagemagick.org</p>
 
 <h5>Citation in Vancouver style</h5>
-<p class="highlight">1. The ImageMagick Development Team. ImageMagick [Internet]. 2020. Available from: https://imagemagick.org</p>
+<p class="highlight">1. The ImageMagick Development Team. ImageMagick [Internet]. 2021. Available from: https://imagemagick.org</p>
 
 <h5>Citation in Harvard style</h5>
-<p class="highlight">The ImageMagick Development Team, 2020. ImageMagick, Available at: https://imagemagick.org.</p>
+<p class="highlight">The ImageMagick Development Team, 2021. ImageMagick, Available at: https://imagemagick.org.</p>
 <h5>Citation in Bibtex format</h5>
 <div class="highlight">
 @software{imagemagick,
@@ -17,6 +17,6 @@
 <br>&nbsp;&nbsp;title&nbsp;=&nbsp;{ImageMagick},
 <br>&nbsp;&nbsp;url&nbsp;=&nbsp;{https://imagemagick.org},
 <br>&nbsp;&nbsp;version&nbsp;=&nbsp;{7.0.10},
-<br>&nbsp;&nbsp;date&nbsp;=&nbsp;{2020-06-12},
+<br>&nbsp;&nbsp;date&nbsp;=&nbsp;{2021-01-04},
 <br>}
 </div>
