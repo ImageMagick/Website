780922faf7a8692946413cd0dbbf26abc8591271
Dirk Lemstra <dirk@lemstra.org>
2020-12-31 10:40:04 +0100

    Added newlines.

--- include/command-line-options.php
+++ include/command-line-options.php
@@ -7037,9 +7037,13 @@ information if it is unwanted.</p>
 single pixel transparent 'missed' image is returned, in the same way as when a
 <a href="#crop">-crop</a> operation 'misses' the image proper. </p>
 
-<p>Use <code><a href="#define">-define</a> trim:percent-background=0%</code> to remove all the background from the image.  The amount of background that is tolerated in an edge specified as a percent.  0% means no background is tolerated.  50% means an edge can contain up to 50% pixels that are background per the fuzz-factor.</p>
+<p>Use <code><a href="#define">-define</a> trim:percent-background=0%</code> to
+remove all the background from the image.  The amount of background that is
+tolerated in an edge specified as a percent.  0% means no background is tolerated.
+50% means an edge can contain up to 50% pixels that are background per the fuzz-factor.</p>
 
-<p>Use <code><a href="#background">-background</a> <em>background-color</em></code> to identify background color surrounding the region of interest.</p>
+<p>Use <code><a href="#background">-background</a> <em>background-color</em></code> to
+identify background color surrounding the region of interest.</p>
 
 <div style="margin: auto;">
   <h3><a class="anchor" id="type"></a>-type <var>type</var></h3>
