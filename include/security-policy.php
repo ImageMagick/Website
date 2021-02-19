83f452c27e422d209ebd6b1bf8075768402ad489
Cristy <urban-warrior@imagemagick.org>
2021-02-19 00:42:36 +0000

    ...

--- include/security-policy.php
+++ include/security-policy.php
@@ -9,7 +9,7 @@
 
 <p>As an example, suppose you download an image from the internet and unbeknownst to you its been crafted to generate a 20000 by 20000 pixel image. ImageMagick attempts to allocate enough resources (memory, disk) and your system will likely deny the resource request and exit. However, its also possible that your computer might be temporarily sluggish or unavailable or ImageMagick may abort. To prevent such a scenario, you can set limits in the <a href="<?php echo $_SESSION['RelativePath']?>/../source/policy.xml">policy.xml</a> configuration file. You might wonder why ImageMagick does not already include reasonable limits? Simply because what is reasonable in your environment, might not be reasonable to someone else. For example, you may have ImageMagick sandboxed where security is not a concern, whereas another user may use ImageMagick to process images on their publically accessible website.  Or ImageMagick runs on a host with 1TB of memory whereas another ImageMagick instance runs on an mobile phone. By policy, permitting giga-pixel image processing on the large memory host makes sense, not so much for the resource constrained mobile phone. If you utilize ImageMagick from a public website, you may want to increase security by preventing usage of the MVG or HTTPS coders. Only you can decide what are reasonable limits taking in consideration your environment. We provide this policy with reasonable limits and encourage you to modify it to suit your local environment:</p>
 
-<pre class="pre-scrollable highlight"><code>&lt;policymap>
+<ul><pre class="pre-scrollable highlight"><code>&lt;policymap>
   &lt;!-- temporary path must be a preexisting writable directory -->
   &lt;policy domain="resource" name="temporary-path" value="/data/magick"/>
   &lt;policy domain="resource" name="memory" value="256MiB"/>
@@ -30,7 +30,7 @@
   &lt;policy domain="cache" name="shared-secret" value="<em>passphrase</em>" stealth="True"/>
   &lt;policy domain="system" name="precision" value="6"/>
   &lt;policy domain="system" name="shred" value="1"/>
-&lt;/policymap></code></pre>
+&lt;/policymap></code></pre></ul>
 
 <p>Since we process multiple simultaneous sessions, we do not want any one session consuming all the available memory. With this policy, large images are cached to disk. If the image is too large and exceeds the pixel cache disk limit, the program exits. In addition, we place a time limit to prevent any run-away processing tasks. If any one image has a width or height that exceeds 8192 pixels or if an image sequence exceeds 32 frames, an exception is thrown and processing stops. As of ImageMagick 7.0.1-8, you can prevent the use of any delegate or all delegates (set the pattern to "*"). Note, prior to these releases, use a domain of <code>coder</code> to prevent delegate usage (e.g. <code>domain="coder" rights="none" pattern="HTTPS"</code>). We prevent users from executing any image filters.  The policy also prevents indirect reads. If you want to, for example, read text from a file (e.g. <code>caption:@myCaption.txt</code>), you'll need to disable the <code>path</code> policy.</p>
 
@@ -38,43 +38,43 @@
 
 <p>Here is what you can expect when you restrict the HTTPS coder, for example:</p>
 
-<pre class="highlight">-> convert https://imagemagick.org/image/wizard.png wizard.jpg
+<ul><pre class="highlight">-> convert https://imagemagick.org/image/wizard.png wizard.jpg
 convert: attempt to perform an operation not allowed by the security policy `HTTPS'
-convert: no images defined `wizard.jpg'</pre>
+convert: no images defined `wizard.jpg'</pre></ul>
 
 <p>As of ImageMagick version 7.0.4-7, you can conveniently deny access to all delegates and coders except for a small subset of proven web-safe image types.  For example,</p>
 
-<pre class="highlight"><code>&lt;policy domain="delegate" rights="none" pattern="*" />
+<ul><pre class="highlight"><code>&lt;policy domain="delegate" rights="none" pattern="*" />
 &lt;policy domain="module" rights="none" pattern="*" />
-&lt;policy domain="module" rights="read | write" pattern="{GIF,JPEG,PNG,WEBP}" /></code></pre>
+&lt;policy domain="module" rights="read | write" pattern="{GIF,JPEG,PNG,WEBP}" /></code></pre></ul>
 
 <p>Here we disable reading just a few Postscript related formats, you can still write them:</p>
-<pre class="highlight"><code>&lt;policy domain="module" rights="write" pattern="{PDF,PS,PS2,PS3,XPS}" /></code></pre>
+<ul><pre class="highlight"><code>&lt;policy domain="module" rights="write" pattern="{PDF,PS,PS2,PS3,XPS}" /></code></pre></ul>
 
 <p>As of ImageMagick 7.0.7-0, you can allocate the pixel cache and some internal buffers with anonymous memory mapping rather than from heap.  As a consequence, the pixels are initialized to zero resulting in a minor performance penalty.  You can also securely delete any temporary files for increased security.  The value is the number of times to shred (replace its content with random data) before deleting a temporary file.  For example,</p>
-<pre class="highlight"><code>&lt;policy domain="system" name="memory-map" value="anonymous"/>
+<ul><pre class="highlight"><code>&lt;policy domain="system" name="memory-map" value="anonymous"/>
 &lt;policy domain="cache" name="memory-map" value="anonymous"/>
-&lt;policy domain="system" name="shred" value="1"/></code></pre>
+&lt;policy domain="system" name="shred" value="1"/></code></pre></ul>
 
 <p>Some image processing algorithms (e.g. wavelet transform) might consume a substantial amount of memory to complete.  ImageMagick maintains a separate memory pool for these large resource requests and as of 7.0.6-1 permits you to set a maximum request limit.  If the limit is exceeded, the allocation is instead memory-mapped on disk.  Here we limit the maximum memory request by policy:</p>
-<pre class="highlight"><code>&lt;policy domain="system" name="max-memory-request" value="256MiB"/> </code></pre>
+<ul><pre class="highlight"><code>&lt;policy domain="system" name="max-memory-request" value="256MiB"/> </code></pre></ul>
 
 <p>As of ImageMagick version 7.0.4-23, you can limit the maximum number of images in a sequence.  For example, to limit an image sequence to at most 64 frames, use:</p>
-<pre class="highlight"><code>&lt;policy domain="resource" name="list-length" value="64"/></code></pre>
+<ul><pre class="highlight"><code>&lt;policy domain="resource" name="list-length" value="64"/></code></pre></ul>
 
 <p>For additional details about resource limits and the policy configuration file, read <a href="<?php echo $_SESSION['RelativePath']?>/../script/resources.php">Resources</a> and <a href="<?php echo $_SESSION['RelativePath']?>/../script/architecture.php">Architecture</a>.</p>
 
 <p>As of ImageMagick 7.0.6-0, you can programmatically set the ImageMagick security policy with SetMagickSecurityPolicy() (MagickCore) or MagickSetSecurityPolicy() (MagickWand).</p>
 
 <p>As of ImageMagick version 7.0.8-11, you can set a module security policy.  For example, to prevent Postscript or PDF interpretation, use:</p>
-<pre class="highlight"><code>&lt;policy domain="module" rights="none" pattern="{ps,pdf,xps}/></code></pre>
+<ul><pre class="highlight"><code>&lt;policy domain="module" rights="none" pattern="{ps,pdf,xps}/></code></pre></ul>
 
 <p>As of ImageMagick version 7.0-10-52, you can set a font policy.  Specify a path to a Unicode font that ImageMagick defaults to whenever the user does not specify a font preference:</p>
-<pre class="highlight"><code>&lt;policy domain="system" name="font" value="/usr/share/fonts/arial-unicode.ttf"/></code></pre>
+<ul><pre class="highlight"><code>&lt;policy domain="system" name="font" value="/usr/share/fonts/arial-unicode.ttf"/></code></pre></ul>
 
 <p>You can verify your policy changes are in effect with this command:</p>
 
-<pre class="pre-scrollable highlight">-> identify -list policy
+<ul><pre class="pre-scrollable highlight">-> identify -list policy
 Path: ImageMagick-7/policy.xml
   Policy: Cache
     name: memory-map
@@ -133,16 +133,16 @@ Path: ImageMagick-7/policy.xml
 
 Path: [built-in]
   Policy: Undefined
-    rights: None</pre>
+    rights: None</pre></ul>
 <p>Notice the <code>shared-secret</code> policy is not listed due to the <code>stealth</code> property.</p>
 
 <h2><a class="anchor" id="synchronize"></a>Pixel Cache Synchronize Policy</h2>
 
 <p>When writing image pixels to disk, ImageMagick firsts preallocates the disk file, which is much faster than fully populating the file with zeros.  To further increase performance, we memory-map the file on disk.  With memory-mapping, we get an increase in performance (up to 5x), however, there remains a possibility that as the disk file is populated, it may run out of free space.  The OS then throws a SIGBUS signal which prevents ImageMagick from continuing.  To prevent a SIGBUS, use this security policy:
 
-<pre class="highlight">
+<ul><pre class="highlight">
 &lt;policy domain="cache" name="synchronize" value="True"/>
-</pre>
+</pre></ul>
 
 <p>Set to True to ensure all image data is fully flushed and synchronized to disk. There is a performance penalty, however, the benefits include ensuring a valid image file in the event of a system crash and early reporting if there is not enough disk space for the image pixel cache.</p>
 
@@ -150,11 +150,11 @@ Path: [built-in]
 
 <p>A zero configuration build of ImageMagick does not permit external configuration files.  To define your security policy, you must instead edit the <code>MagickCore/policy-private.h</code> source module, add your policy statements, and then build the ImageMagick distribution.  Here is an example zero configuration security policy:</p>
 
-<pre class="highlight"><code>static const char
+<ul><pre class="highlight"><code>static const char
   *ZeroConfigurationPolicy = \
 "&lt;policymap> \
   &lt;policy domain=\"coder\" rights=\"none\" pattern=\"MVG\"/> \
-&lt;/policymap>";</code></pre>
+&lt;/policymap>";</code></pre></ul>
 
 <h2><a class="anchor" id="other"></a>Other Security Considerations</h2>
 
