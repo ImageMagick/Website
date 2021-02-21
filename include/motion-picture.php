df8db0fe2f146cf813f686c3e5a67284e21e160d
Cristy <urban-warrior@imagemagick.org>
2021-02-21 15:29:16 +0000

    ...

--- include/motion-picture.php
+++ include/motion-picture.php
@@ -36,7 +36,7 @@
 
 <p>ImageMagick supports these DPX properties:</p>
 
-<pre class="pre-scrollable highlight"><code>dpx:file.copyright
+<ul><pre class="pre-scrollable highlight"><code>dpx:file.copyright
 dpx:file.creator
 dpx:file.filename
 dpx:file.project
@@ -82,35 +82,35 @@ dpx:television.video_signal
 dpx:television.white_level
 dpx:user.id
 dpx:user.data
-</code></pre>
+</code></pre></ul>
 
 <p>Look for any user data as the <code>dpx:user-data</code> image profile.</p>
 
 <p> To determine which properties are associated with your DPX image, use this command for example:</p>
 
-<pre class="highlight"><code>magick identify -verbose bluebells.dpx
-</code></pre>
+<ul><pre class="highlight"><code>magick identify -verbose bluebells.dpx
+</code></pre></ul>
 
 <p>To identify a particular property, try this:</p>
 
-<pre class="highlight"><code>magick identify -format "%[dpx:television.time.code]" bluebells.dpx
-</code></pre>
+<ul><pre class="highlight"><code>magick identify -format "%[dpx:television.time.code]" bluebells.dpx
+</code></pre></ul>
 
 <p>Finally, to set a property:</p>
 
-<pre class="highlight"><code>magick bluebells.dpx -define dpx:television.time.code=10:00:02:15 bluebells-001.dpx
-</code></pre>
+<ul><pre class="highlight"><code>magick bluebells.dpx -define dpx:television.time.code=10:00:02:15 bluebells-001.dpx
+</code></pre></ul>
 
 <h2><a class="anchor" id="settings"></a>DPX Settings</h2>
 
 <p>Use <a href="<?php echo $_SESSION['RelativePath']?>/../script/command-line-options.php#set">-set</a> to specify the image or film gamma or black and white points.  For example use:
 </p>
 
-<pre class="highlight"><code>-set gamma 1.7
+<ul><pre class="highlight"><code>-set gamma 1.7
 -set film-gamma 0.6
 -set reference-black 95
 -set reference-white 685
 -set profile dpx:user.data
-</code></pre>
+</code></pre></ul>
 
 </div>
