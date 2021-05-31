50abdfc35830b10b317ce1382a8db45d098238b1
Cristy <urban-warrior@imagemagick.org>
2021-05-31 20:29:01 +0000

    ...

--- include/magick-cache.php
+++ include/magick-cache.php
@@ -5,17 +5,17 @@
 <p>The Magick Cache works in concert with <a href="https://imagemagick.org">ImageMagick</a>.  Download the <a href="https://github.com/ImageMagick/MagickCache">MagickCache</a> and install.  You'll now want to create the cache and populate it with images, video, and associated metadata.</p>
 <h5>Create a Magick Cache</h5>
 <ul><pre class="bg-light text-dark"><code>$ magick-cache create /opt/magick-cache</code></pre></ul>
-<p>Once you create the magick cache, you will want to populate it with resources including images, video, audio, or metadata.
-<h5>Put an image in the Magick Cache</h5>
+<p>Once you create the magick cache, you will want to populate it with content that includes images, video, audio, or metadata.
+<h5>Put Content in the Magick Cache</h5>
 <ul><pre class="bg-light text-dark"><code>$ magick-cache put /opt/magick-cache movies/image/mission-impossible/cast/rebecca-ferguson 0200508-rebecca-ferguson.jpg</code></pre></ul>
 <p>Note, image identifier is an IRI composed of <em>project</em>/<em>type</em>/<em>resource-path</em>.  In this example, the project is <code>movies</code>, type is <code>image</code>, and the resource path is <code>mission-impossible/cast/rebecca-ferguson</code>.  The path uniquely identifies a resource.  Two different images cannot be stored with the same resource path.  Instead use something like <code>mission-impossible/cast/200508-rebecca-ferguson</code> and <code>mission-impossible/cast/200513-rebecca-ferguson</code>.</p>
 <p>Set a cache key and the time to live to 2 days.  Anytime after 1 day, the image will automatically expire with the <code>expire</code> function.  To get, expire, or delete the image, you will need to use the same cache key.</p>
 <ul><pre class="bg-light text-dark"><code>$ magick-cache -key s5hPjbxEwS -ttl "2 days" put /opt/magick-cache movies/image/mission-impossible/cast/rebecca-ferguson 0200508-rebecca-ferguson.jpg</code></pre></ul>
 <p>Don't forget your cache key.  Without it, you will not be able to get, list, delete or expire your content.</p>
-<h5>Get an image from the Magick Cache</h5>
+<h5>Get Content from the Magick Cache</h5>
 <ul><pre class="bg-light text-dark"><code>$ magick-cache -key s5hPjbxEwS get /opt/magick-cache movies/image/mission-impossible/cast/rebecca-ferguson rebecca-ferguson.png</code></pre></ul>
 <p>Notice the original image was put in the Magick Cache in the JPEG format.  Here we the the image and convert it to the PNG image format.</p>
-<h5>Delete an image from the Magick Cache</h5>
+<h5>Delete Content from the Magick Cache</h5>
 <ul><pre class="bg-light text-dark"><code>$ magick-cache -key s5hPjbxEwS delete /opt/magick-cache movies/image/mission-impossible/cast/rebecca-ferguson</code></pre></ul>
 <p>To delete any cast images that have expired (exceeded their respective time to live), try this comand:</p>
 <ul><pre class="bg-light text-dark"><code>$ magick-cache -key s5hPjbxEwS expire /opt/magick-cache movies/image/mission-impossible/cast</code></pre></ul>
