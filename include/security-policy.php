bf1478c892302260593f82721d3f3b825526a094
Cristy <urban-warrior@imagemagick.org>
2021-09-11 10:59:47 +0000

    ...

--- include/security-policy.php
+++ include/security-policy.php
@@ -22,8 +22,8 @@
   &lt;policy domain="resource" name="file" value="768"/>
   &lt;policy domain="resource" name="thread" value="2"/>
   &lt;policy domain="resource" name="time" value="120"/>
-  &lt;policy domain="module" rights="write" pattern="{MVG,PS,PDF,URL,XPS}" />
-  &lt;policy domain="coder" rights="write" pattern="{MSVG}" />
+  &lt;policy domain="module" rights="none" pattern="{URL}" /> 
+  &lt;policy domain="coder" rights="write" pattern="{MSVG,MVG,PS,PDF,XPS}" />
   &lt;policy domain="filter" rights="none" pattern="*" />
   &lt;policy domain="path" rights="none" pattern="@*"/>  &lt;!-- indirect reads not permitted -->
   &lt;policy domain="cache" name="memory-map" value="anonymous"/>
@@ -49,8 +49,8 @@ convert: no images defined `wizard.jpg'</pre></ul>
 &lt;policy domain="module" rights="none" pattern="*" />
 &lt;policy domain="module" rights="read | write" pattern="{GIF,JPEG,PNG,WEBP}" /></samp></pre></ul>
 
-<p>Here we disable reading just a few Postscript related formats, you can still write them:</p>
-<ul><pre class="bg-light text-dark"><samp>&lt;policy domain="module" rights="write" pattern="{PDF,PS,PS2,PS3,XPS}" /></samp></pre></ul>
+<p>The module policy enables or disables a complete module for both read or write.  To just read or write an image format, use the `coder` policy instead.  For example, we disable reading just a few Postscript related formats, you can still write them:</p>
+<ul><pre class="bg-light text-dark"><samp>&lt;policy domain="coder" rights="write" pattern="{PDF,PS,PS2,PS3,XPS}" /></samp></pre></ul>
 
 <p>As of ImageMagick 7.0.7-0, you can allocate the pixel cache and some internal buffers with anonymous memory mapping rather than from heap.  As a consequence, the pixels are initialized to zero resulting in a minor performance penalty.  You can also securely delete any temporary files for increased security.  The value is the number of times to shred (replace its content with random data) before deleting a temporary file.  For example,</p>
 <ul><pre class="bg-light text-dark"><samp>&lt;policy domain="system" name="memory-map" value="anonymous"/>
