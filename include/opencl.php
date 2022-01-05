17d6bae1c85ce0a67f17a5ec4c8c253352f7e96f
Dirk Lemstra <dirk@lemstra.org>
2022-01-05 08:24:52 +0100

    Corrected documentation for Windows.

--- include/opencl.php
+++ include/opencl.php
@@ -24,7 +24,7 @@
   grayscale
   motion-blur
   resize</pre></ul>
-        The following operators are OpenCL accelerated as well, but they require alpha channel to be enabled, e.g. <samp>MAGICK_OCL_DEVICE=true magick sample.jpg -alpha on -despeckle null:</samp>.
+        The following operators are OpenCL accelerated as well, but they require alpha channel to be enabled, e.g. <samp>magick sample.jpg -alpha on -despeckle null:</samp>.
 <ul><pre class="bg-light text-dark">  despeckle
   equalize
   modulate</pre></ul>
@@ -33,8 +33,9 @@
 
 <h4>OpenCL Operation Notes</h4>
 
-<p>When some of the OpenCL accelerated operators are invoked for the first time and all the conditions above are met, ImageMagick conducts a series of tests to configure the OpenCL environment according to the system hardware; therefore, it is normal to experience a higher latency the first time you use an accelerated option.  The OpenCL kernels are embedded inside ImageMagick in source format.  During the initial setup, ImageMagick discovers all the available OpenCL devices and compiles the kernels for each of these targets.  ImageMagick also runs several performance tests internally to determine the preferred device to use.  The compiled kernel code and the performance test results are stored into the cache directory so that the data can be reused by subsequent invocations of the OpenCL path.  By default, the OpenCL cached data is stored in <samp>$HOME/.cache/ImageMagick</samp> on Linux and on MacOSX or in <samp>%LOCALAPPDATA%\ImageMagick</samp> on Windows. To run these tests for the first time, use the comand below:</p>
-<ul><pre class="bg-light text-dark"><samp>  MAGICK_OCL_DEVICE=true magick sample.jpg -blur 0x5 null:</samp></pre></ul>
+<p>When some of the OpenCL accelerated operators are invoked for the first time and all the conditions above are met, ImageMagick conducts a series of tests to configure the OpenCL environment according to the system hardware; therefore, it is normal to experience a higher latency the first time you use an accelerated option.  The OpenCL kernels are embedded inside ImageMagick in source format.  During the initial setup, ImageMagick discovers all the available OpenCL devices and compiles the kernels for each of these targets.  ImageMagick also runs several performance tests internally to determine the preferred device to use.  The compiled kernel code and the performance test results are stored into the cache directory so that the data can be reused by subsequent invocations of the OpenCL path.  By default, the OpenCL cached data is stored in <samp>$HOME/.cache/ImageMagick</samp> on Linux and on MacOSX or in <samp>%LOCALAPPDATA%\ImageMagick</samp> on Windows. To run these tests for the first time, use the command below:</p>
+<ul><pre class="bg-light text-dark"><samp>MAGICK_OCL_DEVICE=true magick sample.jpg -blur 0x5 null:</samp></pre></ul>
+<p>On Windows the <samp>MAGICK_OCL_DEVICE=true<samp> part should be removed and the command <samp>set MAGICK_OCL_DEVICE=true</samp> should be executed before executing the command above to enable OpenCL operations.</p>
 <p>To change the cache directory, set the MAGICK_OPENCL_CACHE_DIR environment variable.  ImageMagick is able to detect hardware changes, driver updates, and new kernel sources and re-run the setup and the calibration test.  You can also force ImageMagick to re-run the process by removing the content from the cache directory.</p>
 <p>In addition to the environment variables mentioned above, ImageMagick provides a set of APIs that allow developers to gain more fine-grained control of the OpenCL acceleration. For example, use the <samp>InitializeOpenCL()</samp> function to initialize the OpenCL environment:</p>
 
