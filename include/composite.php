2f0732a2029e8a9cddb09cc134eafb0be19a523e
Cristy <urban-warrior@imagemagick.org>
2022-01-08 21:42:08 +0000

    ...

--- include/composite.php
+++ include/composite.php
@@ -37,10 +37,10 @@ magick composite -compose atop -geometry -13-17 white-highlight.png red-circle.p
 <ul><pre class="bg-light text-dark"><samp>magick -verbose stream.jpg bear.jpg bear_mask.png -define compose:args=400x0.0002+100 \
   -compose seamless-blend -geometry +30+30 -composite bear-in-stream.png </samp></pre></ul>
 <p>The mask marks the area around the bear to blend.  Seamless blending is an iterative process.  Here, we limit the iterations to 400 or less if the blending converges (residual has an RMSE of less than 0.0002). The residual value (RMSE) is printed every 100 iterations.  Note, seamless blending works most effectively when the HDRI feature is enabled.</p>
-<ul><pre class="bg-light text-dark"><samp>magick -verbose stream.jpg /( bear.jpg -read-mask only_bear.png \) bear_mask.png \
+<ul><pre class="bg-light text-dark"><samp>magick -verbose stream.jpg \( bear.jpg -read-mask only_bear.png \) bear_mask.png \
   -define compose:args=400x0.0002+100 -compose seamless-blend -geometry +30+30 -composite \
   bear-in-stream.png </samp></pre></ul>
-<p>Here we create read mask that marks the foreground object from its background.  No blending is applied to the foreground object, just its surroundings.</p>
+<p>Here we create read mask that marks the foreground object from its background. No blending is applied to the foreground object, just its surroundings. The read-mask preserves the colors of the bear and prevents them from lightening as in the first example. Note that read-masks must be black on white background as opposed to normal masks, which are white on black background.</p>
 
 <p>You can find additional examples of using <samp>composite</samp> in <a href="https://legacy.imagemagick.org/Usage/">Examples of ImageMagick Usage</a>.  You can find out more about them and the mathematics by looking at <a href="http://www.w3.org/TR/SVG12/rendering.html">SVG Alpha Compositing</a></p>
 
