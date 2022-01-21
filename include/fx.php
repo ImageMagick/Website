86ced638b4c2b5e8adfacd8fc253b7cf8030b56e
Cristy <urban-warrior@imagemagick.org>
2022-01-21 03:02:26 +0000

    ...

--- include/fx.php
+++ include/fx.php
@@ -58,7 +58,7 @@
 
 <p>This FX expression adds random noise to an image:</p>
 
-</ul><pre class="bg-light text-dark"><samp>magick photo.jpg -fx 'iso=32; rone=rand(); rtwo=rand(); \
+<ul><pre class="bg-light text-dark"><samp>magick photo.jpg -fx 'iso=32; rone=rand(); rtwo=rand(); \
   myn=sqrt(-2*ln(rone))*cos(2*Pi*rtwo); myntwo=sqrt(-2*ln(rtwo))* \
   cos(2*Pi*rone); pnoise=sqrt(p)*myn*sqrt(iso)* \
   channel(4.28,3.86,6.68,0)/255; max(0,p+pnoise)' noisy.png
