38b712ecb17be95c37b60505273158052c0c75b3
Cristy <urban-warrior@imagemagick.org>
2022-05-30 20:04:21 +0000

    authoritative change log

--- include/mirror.php
+++ include/mirror.php
@@ -1,6 +1,6 @@
 <div class="magick-header">
 <h1 class="text-center">Mirrors</h1>
-<p class="lead magick-description">ImageMagick source and binary distributions are available from a variety of FTP and Web mirrors around the world listed below.  ImageMagick stable and development source releases are also available from <a href="https://github.com/ImageMagick">Git</a>.  Before you download, you may want to review recent <a href="https://github.com/ImageMagick/Website/blob/main/ChangeLog.md">changes</a> to the ImageMagick distribution.  The authoritative source code repository is <a href="https://github.com/ImageMagick">https://github.com/ImageMagick</a>.</p>
+<p class="lead magick-description">ImageMagick source and binary distributions are available from a variety of FTP and Web mirrors around the world listed below.  ImageMagick stable and development source releases are also available from <a href="https://github.com/ImageMagick">Git</a>.  Before you download, you may want to review recent <a href="https://github.com/ImageMagick/ImageMagick/blob/main/ChangeLog.md">changes</a> to the ImageMagick distribution.  The authoritative source code repository is <a href="https://github.com/ImageMagick">https://github.com/ImageMagick</a>.</p>
 <p>The latest release of ImageMagick is version <?php echo(MagickLibVersionText.MagickLibSubversion); ?>.</p>
 <dl>
   <dt>Germany</dt>
