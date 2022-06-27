a703904d0504487cab03e21b3138cb03952e95db
Cristy <urban-warrior@imagemagick.org>
2022-06-27 12:28:14 -0400

    ...

--- include/advanced-linux-installation.php
+++ include/advanced-linux-installation.php
@@ -421,7 +421,7 @@ identify logo.gif</samp></pre>
 
 <h2><a class="anchor" id="linux"></a>Linux-specific Build instructions</h2>
 
-<p>Download <a href="https://imagemagick.org/archive/linux/SRPMS/ImageMagick.src.rpm">ImageMagick.src.rpm</a> from <a href="https://imagemagick.org/archive">ftp.imagemagick.org</a> or its <a href="<?php echo $_SESSION['RelativePath']?>/../script/download.php">mirrors</a> and verify the distribution against its <a href="https://imagemagick.org/archive/linux/SRPMS/digest.rdf">message digest</a>.</p>
+<p>Download <a href="https://imagemagick.org/archive/linux/SRPMS/ImageMagick.src.rpm">ImageMagick.src.rpm</a> from <a href="https://imagemagick.org/archive">imagemagick.org</a> or its <a href="<?php echo $_SESSION['RelativePath']?>/../script/download.php">mirrors</a> and verify the distribution against its <a href="https://imagemagick.org/archive/linux/SRPMS/digest.rdf">message digest</a>.</p>
 
 <p>Build ImageMagick with this command:</p>
 
