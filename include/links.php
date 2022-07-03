33685c920a3290d41c4b164c168f61d0a1248c2c
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2022-07-03 16:31:12 -0700

    Update Android repo links

--- include/links.php
+++ include/links.php
@@ -81,7 +81,7 @@
 <h2><a class="anchor" id="other-projects"></a>Other Projects</h2>
 <ul>
   <dd class="col-md-8"><a href="https://urban-warrior.org/">Wizard's Toolkit</a></dd>
-  <dd class="col-md-8"><a href="https://github.com/cherryleafroad/Android-ImageMagick7">ImageMagick API on the Android Platform</a></dd>
+  <dd class="col-md-8"><a href="https://github.com/MolotovCherry/Android-ImageMagick7">ImageMagick API on the Android Platform</a></dd>
   <dd class="col-md-8"><a href="https://imagemagick.org/archive/iOS/">ImageMagick API on the iOS / iPhone  Platform</a></dd>
 </ul>
 </div>
