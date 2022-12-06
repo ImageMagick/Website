cad47ea5a50b0895886f9f3ab4ea55077e9ba67a
Cristy <urban-warrior@imagemagick.org>
2022-12-05 20:51:35 -0500

    ...

--- include/define.php
+++ include/define.php
@@ -2,12 +2,12 @@
   /*
     ImageMagick Constants.
   */
-  define('DevelopmentTeam', 'support-magick-0x01@urban-warrior.org');
+  define('DevelopmentTeam', 'support-magick@urban-warrior.org');
   define('SponsorIcon', 'networkredux.png');
   define('SponsorURL', 'https://networkredux.com');
   define('MagickMajorReleaseText', '7');
   define('MagickLibVersionText', '7.1.0');
-  define('MagickLibSubversion', '-37');
+  define('MagickLibSubversion', '-53');
   define('PerlMagickVersion', '7.1.0');
-  define('MagickReleaseDate', '2022-05-30')
+  define('MagickReleaseDate', '2022-11-01')
 ?>
