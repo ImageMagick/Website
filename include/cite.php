13bec40b494c6289f8b767979f26592de18e77c0
Cristy <urban-warrior@imagemagick.org>
2022-12-08 08:36:58 -0500

    ...

--- include/cite.php
+++ include/cite.php
@@ -3,18 +3,18 @@
 <p class="lead magick-description">Learn how to cite ImageMagick in your work.  Choose from these citation formats:</p>
 
 <h4>Citation in APA style</h4>
-<pre class="bg-light text-dark mx-4"><samp>The ImageMagick Development Team. (2021). ImageMagick. Retrieved from https://imagemagick.org</samp></pre>
+<pre class="bg-light text-dark mx-4"><samp>ImageMagick Studio LLC. (2023). ImageMagick. Retrieved from https://imagemagick.org</samp></pre>
 
 <h4>Citation in Vancouver style</h4>
-<pre class="bg-light text-dark mx-4"><samp>1. The ImageMagick Development Team. ImageMagick [Internet]. 2021. Available from: https://imagemagick.org</samp></pre>
+<pre class="bg-light text-dark mx-4"><samp>1. ImageMagick Studio LLC. ImageMagick [Internet]. 2023. Available from: https://imagemagick.org</samp></pre>
 
 <h4>Citation in Harvard style</h4>
-<pre class="bg-light text-dark mx-4"><sampl>The ImageMagick Development Team, 2021. ImageMagick, Available at: https://imagemagick.org.</samp></pre>
+<pre class="bg-light text-dark mx-4"><sampl>ImageMagick Studio LLC, 2023. ImageMagick, Available at: https://imagemagick.org.</samp></pre>
 <h4>Citation in Bibtex format</h4>
 <pre class="bg-light text-dark mx-4"><samp>@software{imagemagick,
-  author = {{The ImageMagick Development Team}},
+  author = {{ImageMagick Studio LLC}},
   title = {ImageMagick},
   url = {https://imagemagick.org},
   version = {7.0.10},
-  date = {2021-01-04},
+  date = {2023-01-04},
 }</samp></pre>
