dd1df98ae8ece64c828cca24e5b4c28078fb9609
Cristy <urban-warrior@imagemagick.org>
2023-07-27 15:16:57 -0400

    ...

--- include/defines.php
+++ include/defines.php
@@ -1291,6 +1291,11 @@ use:</p>
   </tr>
 
   <tr>
+    <td>svg:xml-substitute-entities=<var>true</var></td>
+    <td>Enable entity substitution if you trust the source.</td>
+  </tr>
+
+  <tr>
     <td>svg:xml-parse-huge=<var>true</var></td>
     <td>Enable rendering of a very large SVG for which you trust the source</td>
   </tr>
