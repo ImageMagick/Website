574fcaef35c76019fccddd81cc14bf4a4f5ffd6c
Cristy <urban-warrior@imagemagick.org>
2023-10-24 20:44:31 -0400

    ...

---
+++ script/privacy-policy.php
@@ -0,0 +1,6 @@
+<?php
+  $title='ImageMagick Privacy Policy';
+  $topic='Privacy';
+  $description='This Privacy Policy is designed to help you understand how we collect, use, disclose, and safeguard your personal information when you interact with our software, website, and services.';
+  include('session.php');
+?>
