ed569adf0595cb0b4cee0e51d00d5a5847729dce
Cristy <urban-warrior@imagemagick.org>
2024-02-26 07:32:23 -0500

    ...

--- include/privacy-policy.php
+++ include/privacy-policy.php
@@ -1,6 +1,5 @@
-<?php
-  $title='ImageMagick Privacy Policy';
-  $topic='Privacy';
-  $description='As the organization overseeing the open-source ImageMagick software distribution, ImageMagick Studio LLC does not collect any personal identifier information (PII). In addition to the organization, the ImageMagick software distribution does not collect PII either.';
-  include('session.php');
-?>
+<div class="magick-header">
+<h1 class="text-center">Privacy Policy</h1>
+<p  class="lead magick-description">As the organization overseeing the open-source ImageMagick software distribution, ImageMagick Studio LLC does not collect any personal identifier information (PII). In addition to the organization, the ImageMagick software distribution does not collect PII either.</p>
+
+</div>
--- script/privacy-policy.php
+++ script/privacy-policy.php
@@ -1,6 +1,4 @@
 <?php
-  $title='ImageMagick Privacy Policy';
-  $topic='Privacy';
-  $description='This Privacy Policy is designed to help you understand how we collect, use, disclose, and safeguard your personal information when you interact with our software, website, and services.';
+  $title='Privacy Policy';
   include('session.php');
 ?>
