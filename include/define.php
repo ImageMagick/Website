dcc8148369e94148eab3108464a27002837fa3fc
Dirk Lemstra <dirk@lemstra.org>
2025-02-24 21:18:51 +0100

    Removed unused constants.

--- include/define.php
+++ include/define.php
@@ -3,11 +3,8 @@
     ImageMagick Constants.
   */
   define('DevelopmentTeam', 'support-magick-0x01@urban-warrior.org');
-  define('SponsorIcon', 'networkredux.png');
-  define('SponsorURL', 'https://networkredux.com');
   define('MagickMajorReleaseText', '7');
   define('MagickLibVersionText', '7.1.1');
   define('MagickLibSubversion', '-30');
-  define('PerlMagickVersion', '7.1.1');
   define('MagickReleaseDate', '2024-02-11')
 ?>
