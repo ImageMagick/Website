2bb74811fe616f53650b7814ecc449f507a3c7ee
Dirk Lemstra <dirk@lemstra.org>
2025-02-28 16:04:00 +0100

    Moved function to contact form.

--- include/contact.php
+++ include/contact.php
@@ -1,6 +1,13 @@
 <div>
 <?php
-  require_once($_SESSION['AbsolutePath'] . '/../function/exception.php');
+  function ThrowException($message)
+  {
+    print "  <div id=\"notice\">\n";
+    print "    <h4><img src=\"" . $_SESSION['RelativePath'] .
+      "/../image/icon-notice.gif\" width=\"30\" height=\"24\" border=\"0\" " .
+      "alt=\"\" align=\"top\">$message</h4>\n";
+    print "  </div>\n";
+  }
 
   $action=isset($_POST['action']) ? $_POST['action'] : '';
   $authenticate=isset($_POST['authenticate']) ? htmlspecialchars($_POST['authenticate']) : '';
