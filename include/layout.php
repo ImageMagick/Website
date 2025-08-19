ea5432ec60216b30d30cb43318549aa944cd198e
Cristy <urban-warrior@imagemagick.org>
2025-08-19 12:28:13 -0400

    ...

--- include/layout.php
+++ include/layout.php
@@ -158,7 +158,7 @@
 </div>
 
   <!-- Javascript assets -->
-  <script async src="<?php echo $_SESSION['RelativePath']?>/../assets/bootstrap.bundle.min.js" ></script>
+  <script defer src="<?php echo $_SESSION['RelativePath']?>/../assets/bootstrap.bundle.min.js" ></script>
   </body>
 </html>
 <?php
