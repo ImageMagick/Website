6c357a27b8cd05d0406c754dc951e792a6d3bf63
Cristy <urban-warrior@imagemagick.org>
2026-01-24 10:39:38 -0500

    ...

--- include/montage.php
+++ include/montage.php
@@ -392,6 +392,11 @@ transparent, extract, background, or shape the alpha channel</td>
   </tr>
 
   <tr>
+    <td><a href="<?php echo $_SESSION['RelativePath']?>/../script/command-line-options.php#precision">-precision <var>value</var></a></td>
+    <td>set the maximum number of significant digits to be printed</td>
+  </tr>
+
+  <tr>
     <td><a href="<?php echo $_SESSION['RelativePath']?>/../script/command-line-options.php#profile">-profile <var>filename</var></a></td>
     <td>add, delete, or apply an image profile</td>
   </tr>
