30603f4269c36a69a7aa3e0bdeeea0b751a44b8d
Dirk Lemstra <dirk@lemstra.org>
2026-03-21 10:40:34 +0100

    Removed old index file.

--- script/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $title='Mastering Digital Image Alchemy';
-  $topic='Home';
-  $description='ImageMagick is a powerful open-source software suite for creating, editing, converting, and manipulating images in over 200 formats. Ideal for developers, designers, and researchers.';
-  include('session.php');
-?>
