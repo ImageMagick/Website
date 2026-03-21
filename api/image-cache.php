a29b6cddbfb188752121979be5c3ae5c13acab7c
Dirk Lemstra <dirk@lemstra.org>
2026-03-21 11:06:03 +0100

    Removed api folder.

--- api/image-cache.php
+++
@@ -1,4 +0,0 @@
-<?php
-  $title='MagickCore, C API: Image Cache Methods';
-  include('session.php');
-?>
--- api/vision.php
+++
@@ -1,4 +0,0 @@
-<?php
-  $title='MagickCore, C API: Computer Vision';
-  include('session.php');
-?>
