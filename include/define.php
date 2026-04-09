b716a4fd31a90c245f24b3a04cc92655990a53ba
Dirk Lemstra <dirk@lemstra.org>
2026-04-09 18:07:36 +0200

    Updated the version.

--- include/define.php
+++ include/define.php
@@ -4,7 +4,7 @@
   */
   define('MagickDevelopmentTeam', 'support-magick-0x01@urban-warrior.org');
   define('MagickMajorReleaseText', '7');
-  define('MagickLibVersionText', '7.1.1');
-  define('MagickLibSubversion', '-30');
-  define('MagickReleaseDate', '2024-02-11')
+  define('MagickLibVersionText', '7.1.2');
+  define('MagickLibSubversion', '-18');
+  define('MagickReleaseDate', '2026-03-22')
 ?>
