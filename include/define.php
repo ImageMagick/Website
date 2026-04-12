9dbbe85fd4c5c3e292dfb235c847af4f5b1c0d20
Dirk Lemstra <dirk@lemstra.org>
2026-04-12 17:23:34 +0200

    Removed unused variable.

--- include/define.php
+++ include/define.php
@@ -2,7 +2,6 @@
   /*
     ImageMagick Constants.
   */
-  define('MagickDevelopmentTeam', 'support-magick-0x01@urban-warrior.org');
   define('MagickMajorReleaseText', '7');
   define('MagickLibVersionText', '7.1.2');
   define('MagickLibSubversion', '-19');
