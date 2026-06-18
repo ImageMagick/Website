7db0cfc1cdf4d28fc3ff06529e771fed4b799201
Cristy <urban-warrior@imagemagick.org>
2026-06-18 19:58:03 -0400

    block SVG entity-substitution

--- include/security-policy.php
+++ include/security-policy.php
@@ -146,6 +146,8 @@
   &lt;policy domain="system" name="max-memory-request" value="256MiB"/>
   &lt;-- If the basename of path is a symbolic link, the open fails -->
   &lt;policy domain="system" name="symlink" rights="none" pattern="follow"/>
+  &lt;-- Blocks all SVG entity‑substitution attempts by denying the svg:substitute-entities define -->
+  &lt;policy domain="system" name="svg" rights="none" pattern="substitute-entities"/>
 &lt;/policymap></code></pre>
 
 <p>To prevent one session from consuming all available memory when processing multiple sessions at the same time, large images are cached to disk with this policy. If an image exceeds the pixel cache disk limit, the program will exit. Additionally, a time limit has been set to prevent any processing tasks from running for too long. If an image has a width or height larger than 8192 pixels, or if an image sequence has more than 32 frames, processing will stop and an exception will be thrown.</p>
